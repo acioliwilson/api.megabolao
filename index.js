5e8c256d28a2714a6a294a7552be6769c1fe628e
Wilson Acioli <acioliwilson@gmail.com>
2023-12-02 17:58:25 -0300

    new test ds route

--- index.js
+++ index.js
@@ -57,52 +57,15 @@ app.get('/api/resultado/megasena', async (req, res) => {
     }
 });
 
+const url2 = 'https://www.megaloterias.com.br/dupla-sena/resultados';
 app.get('/api/resultado/duplasena', async (req, res) => {
     try {
-        const response = await axios.get('https://www.megaloterias.com.br/dupla-sena/resultados');
+        const response = await axios.get(url2);
         const html = response.data;
         const $ = cheerio.load(html);
 
         const resultadoInfo = {
-            tituloModalidade: $('div.result__title h2').text().trim(),
-            concurso: Number($('div.result__draw strong').text().trim()),
-            dataSorteio: $('div.result__draw-date strong').text().trim(),
-            localSorteio: $('div.text-sm-left strong').text().trim(),
-            valorPremiado: $('div.result__prize__wrap span.result__prize__value').text().trim(),
-            // primeiroSorteio: {
-            //     numerosSorteados: $('div.lot-bg-light span').slice(0, 6).map(function() {
-            //         return $(this).text().trim();
-            //     }).get()
-            // },
-            // segundoSorteio: {
-            //     numerosSorteados: $('div.result__tens-grid div.lot-bg-light span').slice(6, 12).map(function() {
-            //         return $(this).text().trim();
-            //     }).get()
-            // },
-            // acumulado: (( $('p.text-uppercase strong').text().trim() === 'Acumulou!' )?true:false),
-            // premiacaoPrimeiroSorteio: {
-            //     premiacoes: $('table.result__table-prize tbody tr:not(:first-child)').slice(0, 4).map(function() {
-            //         const premiacao = {
-            //             categoria: $(this).find('td:nth-child(1)').text().trim(),
-            //             ganhadores: $(this).find('td:nth-child(2)').text().trim(),
-            //             premio: $(this).find('td:nth-child(3)').text().trim(),
-            //         };
-            //         return premiacao;
-            //     }).get(),
-            // },
-            // premiacaoSegundoSorteio: {
-            //     premiacoes: $('table.result__table-prize tbody tr:not(:first-child)').slice(4, 8).map(function() {
-            //         const premiacao = {
-            //             categoria: $(this).find('td:nth-child(1)').text().trim(),
-            //             ganhadores: $(this).find('td:nth-child(2)').text().trim(),
-            //             premio: $(this).find('td:nth-child(3)').text().trim(),
-            //         };
-            //         return premiacao;
-            //     }).get(),
-            // },
-            dataProxConcurso: $('.banner-nextdraw__draw-date strong').text().trim(),
-            ProxConcurso: Number($('.banner-nextdraw__draw strong').text().trim()),
-            valorEstimadoProxConcurso: $('.banner-nextdraw__prize__wrap').text().trim()
+            tituloModalidade: $('div.result__title h2').text().trim()
         };
 
         res.json(resultadoInfo);
@@ -111,6 +74,61 @@ app.get('/api/resultado/duplasena', async (req, res) => {
         res.status(500).json({ error: 'Erro interno do servidor' });
     }
 });
+
+// app.get('/api/resultado/duplasena', async (req, res) => {
+//     try {
+//         const response = await axios.get('https://www.megaloterias.com.br/dupla-sena/resultados');
+//         const html = response.data;
+//         const $ = cheerio.load(html);
+
+//         const resultadoInfo = {
+//             tituloModalidade: $('div.result__title h2').text().trim(),
+//             concurso: Number($('div.result__draw strong').text().trim()),
+//             dataSorteio: $('div.result__draw-date strong').text().trim(),
+//             localSorteio: $('div.text-sm-left strong').text().trim(),
+//             valorPremiado: $('div.result__prize__wrap span.result__prize__value').text().trim(),
+//             primeiroSorteio: {
+//                 numerosSorteados: $('div.lot-bg-light span').slice(0, 6).map(function() {
+//                     return $(this).text().trim();
+//                 }).get()
+//             },
+//             segundoSorteio: {
+//                 numerosSorteados: $('div.result__tens-grid div.lot-bg-light span').slice(6, 12).map(function() {
+//                     return $(this).text().trim();
+//                 }).get()
+//             },
+//             acumulado: (( $('p.text-uppercase strong').text().trim() === 'Acumulou!' )?true:false),
+//             premiacaoPrimeiroSorteio: {
+//                 premiacoes: $('table.result__table-prize tbody tr:not(:first-child)').slice(0, 4).map(function() {
+//                     const premiacao = {
+//                         categoria: $(this).find('td:nth-child(1)').text().trim(),
+//                         ganhadores: $(this).find('td:nth-child(2)').text().trim(),
+//                         premio: $(this).find('td:nth-child(3)').text().trim(),
+//                     };
+//                     return premiacao;
+//                 }).get(),
+//             },
+//             premiacaoSegundoSorteio: {
+//                 premiacoes: $('table.result__table-prize tbody tr:not(:first-child)').slice(4, 8).map(function() {
+//                     const premiacao = {
+//                         categoria: $(this).find('td:nth-child(1)').text().trim(),
+//                         ganhadores: $(this).find('td:nth-child(2)').text().trim(),
+//                         premio: $(this).find('td:nth-child(3)').text().trim(),
+//                     };
+//                     return premiacao;
+//                 }).get(),
+//             },
+//             dataProxConcurso: $('.banner-nextdraw__draw-date strong').text().trim(),
+//             ProxConcurso: Number($('.banner-nextdraw__draw strong').text().trim()),
+//             valorEstimadoProxConcurso: $('.banner-nextdraw__prize__wrap').text().trim()
+//         };
+
+//         res.json(resultadoInfo);
+//     } catch (error) {
+//         console.error('Erro ao fazer a requisição:', error);
+//         res.status(500).json({ error: 'Erro interno do servidor' });
+//     }
+// });
 
 const port = process.env.PORT || 3000;
 
